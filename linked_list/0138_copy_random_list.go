2a41a180dede7ddeef598efc5e72de666bdac24c
moulindavid <moulin.david@live.fr>
2023-07-02 13:01:27 +0200

    feat: copy random list

---
+++ linked_list/0138_copy_random_list.go
@@ -0,0 +1,30 @@
+package linkedlist
+
+type Node struct {
+	Val    int
+	Next   *Node
+	Random *Node
+}
+
+func copyRandomList(head *Node) *Node {
+	oldToCopy := make(map[*Node]*Node)
+
+	current := head
+	for current != nil {
+		clone := &Node{Val: current.Val}
+		oldToCopy[current] = clone
+		current = current.Next
+	}
+
+	current = head
+
+	for current != nil {
+		clone := oldToCopy[current]
+		clone.Next = oldToCopy[current.Next]
+		clone.Random = oldToCopy[current.Random]
+
+		current = current.Next
+	}
+
+	return oldToCopy[head]
+}
