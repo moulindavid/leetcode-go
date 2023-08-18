7ef326a7e0a682838a4ed490b4bb510e5e29a32d
moulindavid <moulin.david@live.fr>
2023-08-18 11:03:22 +0200

    feat: add dfs iterative

--- trees/0104-maximum-depth-of-binary-tree.go
+++ trees/0104-maximum-depth-of-binary-tree.go
@@ -1,12 +1,14 @@
 package main
 
+import "container/list"
+
 type TreeNode struct {
 	Val   int
 	Left  *TreeNode
 	Right *TreeNode
 }
 
-// DFS
+// DFS recursive
 func maxDepth(root *TreeNode) int {
 	if root == nil {
 		return 0
@@ -19,4 +21,31 @@ func max(a int, b int) int {
 		return a
 	}
 	return b
+}
+
+// DFS iterative
+func maxDepth2(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+
+	queue := list.New()
+	queue.PushBack(root)
+	level := 0
+	for queue.Len() > 0 {
+		n := queue.Len()
+		for i := 0; i < n; i++ {
+			e := queue.Front()
+			queue.Remove(e)
+			node := e.Value.(*TreeNode)
+			if node.Left != nil {
+				queue.PushBack(node.Left)
+			}
+			if node.Right != nil {
+				queue.PushBack(node.Right)
+			}
+		}
+		level++
+	}
+	return level
 }
